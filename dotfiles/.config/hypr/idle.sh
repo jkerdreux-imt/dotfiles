64f4ccc53903b0288ca77f9b57dd76b1db644e32
jkerdreux-imt <jerome.kerdreux@imt-atlantique.fr>
2025-04-18 17:58:04 +0200

    Hyprland and waybar update
    - Use xAAL devices in waybar
    - Move workspace in local file

--- dotfiles/.config/hypr/idle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-swayidle -w \
-  timeout 300 '~/.config/hypr/lock.sh' \
-  timeout 600 'hyprctl dispatch dpms off' \
-  before-sleep '~/.config/hypr/lock.sh' \
-  after-resume 'hyprctl dispatch dpms on'

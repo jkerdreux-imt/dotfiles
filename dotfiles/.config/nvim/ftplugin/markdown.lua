f3819917c7fe6aefe770e7e7807f0440535d54c3
jkerdreux-imt <jerome.kerdreux@imt-atlantique.fr>
2025-09-14 14:16:34 +0200

    Add support for markdown

---
+++ dotfiles/.config/nvim/ftplugin/markdown.lua
@@ -0,0 +1,5 @@
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "fr,en"
+vim.opt_local.textwidth = 90   -- force le retour à la ligne à 90 colonnes
+vim.opt_local.formatoptions:append("t") -- wrap les paragraphes quand tu tapes
+

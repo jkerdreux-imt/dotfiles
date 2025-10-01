674f411d52c67b20b43e029454ec5541d88cd0e6
jkerdreux-imt <jerome.kerdreux@imt-atlantique.fr>
2025-10-02 00:39:47 +0200

    Fix a bug that produce a 0 file on call

--- dotfiles/.config/hypr/toggle-workspace.fish
+++ dotfiles/.config/hypr/toggle-workspace.fish
@@ -1,23 +1,28 @@
 #!/usr/bin/fish
 
-if test (count $argv) >0
+# Usage: ws <target_workspace_id>
+
+if test (count $argv) -gt 0
     set target_ws $argv[1]
 else
     echo "No target workspace provided"
     exit 1
 end
 
-set current_ws (hyprctl activeworkspace -j |jq -r '.id')
+set current_ws (hyprctl activeworkspace -j | jq -r '.id')
 
-set -q HYPR_LAST_WS || set -U HYPR_LAST_WS 1
+# Initialize HYPR_LAST_WS if not set
+if not set -q HYPR_LAST_WS
+    set -U HYPR_LAST_WS $current_ws
+end
 
-echo "Switching from $current_ws to $target_ws ($HYPR_LAST_WS)"
+echo "Switching from $current_ws to $target_ws (last: $HYPR_LAST_WS)"
 
 if test "$current_ws" = "$target_ws"
-    # Retour au workspace précédent
+    # Toggle back to previous workspace
     hyprctl dispatch workspace $HYPR_LAST_WS
 else
-    # Stocker l'ID du workspace actuel dans HYPR_PREV_WS
-    set -U HYPR_LAST_WS "$current_ws"
+    # Store the current workspace as the last one
+    set -U HYPR_LAST_WS $current_ws
     hyprctl dispatch workspace $target_ws
 end
